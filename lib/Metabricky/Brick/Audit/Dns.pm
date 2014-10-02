685f041653913662e68b4e7732f89db65644a96d
gomor <gomor@localhost>
2014-10-02 18:15:37 +0000

    - test: +"Id Revision"

--- lib/Metabricky/Brick/Audit/Dns.pm
+++ lib/Metabricky/Brick/Audit/Dns.pm
@@ -1,5 +1,5 @@
 #
-# $Id: Dns.pm 89 2014-09-17 20:29:29Z gomor $
+# $Id$
 #
 # audit::dns Brick
 #
@@ -10,7 +10,7 @@ use warnings;
 use base qw(Metabricky::Brick);
 
 sub revision {
-   return '$Revision: 144 $';
+   return '$Revision$';
 }
 
 sub declare_attributes {
