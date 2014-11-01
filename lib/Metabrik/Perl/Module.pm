74deae738291a9795afc7a7e5552a30e00a4e2d5
gomor <gomor@localhost>
2014-11-01 10:27:50 +0000

    - moved: all Briks to remove one level: Metabrik::Brik => Metabrik

---
+++ lib/Metabrik/Perl/Module.pm
@@ -0,0 +1,44 @@
+#
+# $Id$
+#
+# perl::module Brik
+#
+package Metabrik::Brik::Perl::Module;
+use strict;
+use warnings;
+
+use base qw(Metabrik::Brik);
+
+sub brik_properties {
+   return {
+      revision => '$Revision$',
+      tags => [ qw(perl module install cpan) ],
+      commands => {
+         install => [ qw(Module) ],
+      },
+      require_binaries => {
+         'metabrik-cpanm' => [ ],
+      },
+   };
+}
+
+sub install {
+   my $self = shift;
+   my ($module) = @_;
+
+   if (! defined($module)) {
+      return $self->log->info($self->brik_help_run('install'));
+   }
+
+   if ($module !~ /^[A-Za-z0-9:]+$/) {
+      return $self->log->error("install: module [$module]: invalid format");
+   }
+
+   system("metabrik-cpanm $module");
+
+   return 1;
+}
+
+1;
+
+__END__
