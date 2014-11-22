13bc2a07ed9431b84e594538570b724f72c1e335
gomor <gomor@localhost>
2014-11-22 16:09:24 +0000

    - new: files

---
+++ lib/Metabrik/File/Psv.pm
@@ -0,0 +1,41 @@
+#
+# $Id$
+#
+# file::psv Brik
+#
+package Metabrik::File::Psv;
+use strict;
+use warnings;
+
+use base qw(Metabrik::File::Csv);
+
+sub brik_properties {
+   return {
+      revision => '$Revision$',
+      tags => [ qw(unstable psv file) ],
+      attributes_default => {
+         separator => "|",
+      },
+   };
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+Metabrik::File::Psv - file::psv Brik
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (c) 2014, Patrice E<lt>GomoRE<gt> Auffret
+
+You may distribute this module under the terms of The BSD 3-Clause License.
+See LICENSE file in the source distribution archive.
+
+=head1 AUTHOR
+
+Patrice E<lt>GomoRE<gt> Auffret
+
+=cut
---
+++ lib/Metabrik/File/Tsv.pm
@@ -0,0 +1,41 @@
+#
+# $Id$
+#
+# file::tsv Brik
+#
+package Metabrik::File::Tsv;
+use strict;
+use warnings;
+
+use base qw(Metabrik::File::Csv);
+
+sub brik_properties {
+   return {
+      revision => '$Revision$',
+      tags => [ qw(unstable tsv file) ],
+      attributes_default => {
+         separator => "\t",
+      },
+   };
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+Metabrik::File::Tsv - file::tsv Brik
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (c) 2014, Patrice E<lt>GomoRE<gt> Auffret
+
+You may distribute this module under the terms of The BSD 3-Clause License.
+See LICENSE file in the source distribution archive.
+
+=head1 AUTHOR
+
+Patrice E<lt>GomoRE<gt> Auffret
+
+=cut
