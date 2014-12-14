086e6b34d26fb1f6cb90a96732ffc25845248dc0
gomor <gomor@localhost>
2014-12-14 15:40:55 +0000

    - update: lookup::ethernet: use a _lookup method so some Briks can inherit Commands from there.

--- lib/Metabrik/Lookup/Ethernet.pm
+++ lib/Metabrik/Lookup/Ethernet.pm
@@ -21,36 +21,42 @@ sub brik_properties {
    };
 }
 
-my $lookup = {
-   '0x0800' => 'ipv4',
-   '0x0805' => 'x25',
-   '0x0806' => 'arp',
-   '0x2001' => 'cgmp',
-   '0x2452' => '802.11',
-   '0x8021' => 'pppipcp',
-   '0x8035' => 'rarp',
-   '0x809b' => 'ddp',
-   '0x80f3' => 'aarp',
-   '0x80fd' => 'pppccp',
-   '0x80ff' => 'wcp',
-   '0x8100' => '802.1q',
-   '0x8137' => 'ipx',
-   '0x8181' => 'stp',
-   '0x86dd' => 'ipv6',
-   '0x872d' => 'wlccp',
-   '0x8847' => 'mpls',
-   '0x8863' => 'pppoed',
-   '0x8864' => 'pppoes',
-   '0x888e' => '802.1x',
-   '0x88a2' => 'aoe',
-   '0x88c7' => '802.11i',
-   '0x88cc' => 'lldp',
-   '0x88d9' => 'lltd',
-   '0x9000' => 'loop',
-   '0x9100' => 'vlan',
-   '0xc023' => 'ppppap',
-   '0xc223' => 'pppchap',
-};
+sub _lookup {
+   my $self = shift;
+
+   my $lookup = {
+      '0x0800' => 'ipv4',
+      '0x0805' => 'x25',
+      '0x0806' => 'arp',
+      '0x2001' => 'cgmp',
+      '0x2452' => '802.11',
+      '0x8021' => 'pppipcp',
+      '0x8035' => 'rarp',
+      '0x809b' => 'ddp',
+      '0x80f3' => 'aarp',
+      '0x80fd' => 'pppccp',
+      '0x80ff' => 'wcp',
+      '0x8100' => '802.1q',
+      '0x8137' => 'ipx',
+      '0x8181' => 'stp',
+      '0x86dd' => 'ipv6',
+      '0x872d' => 'wlccp',
+      '0x8847' => 'mpls',
+      '0x8863' => 'pppoed',
+      '0x8864' => 'pppoes',
+      '0x888e' => '802.1x',
+      '0x88a2' => 'aoe',
+      '0x88c7' => '802.11i',
+      '0x88cc' => 'lldp',
+      '0x88d9' => 'lltd',
+      '0x9000' => 'loop',
+      '0x9100' => 'vlan',
+      '0xc023' => 'ppppap',
+      '0xc223' => 'pppchap',
+   };
+
+   return $lookup;
+}
 
 sub hex {
    my $self = shift;
@@ -66,7 +72,7 @@ sub hex {
    }
    $hex = sprintf("0x%04s", $hex);
 
-   return $lookup->{$hex} || 'unknown';
+   return $self->_lookup->{$hex} || 'unknown';
 }
 
 sub int {
@@ -92,6 +98,8 @@ sub string {
    if (! defined($string)) {
       return $self->log->error($self->brik_help_run('string'));
    }
+
+   my $lookup = $self->_lookup;
 
    my $rev = {};
    while (my ($key, $val) = each(%$lookup)) {
