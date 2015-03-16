a6a6195c405d632b206b88e1810d1d388d1fcd2e
GomoR <gomor@metabrik.org>
2015-03-16 07:41:45 +0100

    - UPDATE: network::arp: merge scan Command from network::arpdiscovery

--- lib/Metabrik/Network/Arp.pm
+++ lib/Metabrik/Network/Arp.pm
@@ -7,22 +7,34 @@ package Metabrik::Network::Arp;
 use strict;
 use warnings;
 
-use base qw(Metabrik);
+use base qw(Metabrik::Network::Frame);
 
 sub brik_properties {
    return {
       revision => '$Revision$',
-      tags => [ qw(unstable arp cache poison eui64) ],
+      tags => [ qw(unstable arp cache poison eui64 discover scan) ],
       attributes => {
+         try => [ qw(try_count) ],
+         timeout => [ qw(timeout_seconds) ],
          _dnet => [ qw(Net::Libdnet::Arp) ],
+      },
+      attributes_default => {
+         try => 2,
+         timeout => 2,
       },
       commands => {
          cache => [ ],
          half_poison => [ ],
          full_poison => [ ],
          mac2eui64 => [ qw(mac_address) ],
+         scan => [ qw(subnet|OPTIONAL) ],
       },
       require_modules => {
+         'Metabrik::Network::Arp' => [ ],
+         'Metabrik::Network::Write' => [ ],
+         'Metabrik::Network::Read' => [ ],
+         'Metabrik::Network::Address' => [ ],
+         'Net::Frame::Layer::ARP' => [ ],
          'Net::Libdnet::Arp' => [ ],
       },
    };
@@ -93,6 +105,130 @@ sub mac2eui64 {
 
    return sprintf("fe80::%x%x:%xff:fe%x:%x%x", $b0, hex($b[1]), hex($b[2]),
       hex($b[3]), hex($b[4]), hex($b[5]));
+}
+
+sub _get_arp_frame {
+   my $self = shift;
+   my ($dst_ip) = @_;
+
+   my $eth = $self->eth;
+   $eth->type(0x0806);  # ARP
+
+   my $arp = $self->arp($dst_ip);
+   my $frame = $self->frame([ $eth, $arp ]);
+
+   return $frame;
+}
+
+sub scan {
+   my $self = shift;
+   my ($subnet) = @_;
+
+   if ($< != 0) {
+      return $self->log->error("scan: must be root to run");
+   }
+
+   my $network_address = Metabrik::Network::Address->new_from_brik_init($self) or return;
+
+   my $interface = $self->device_info;
+
+   $subnet ||= $interface->{subnet};
+
+   my $arp_cache = $self->cache
+      or return $self->log->error("scan: cache failed");
+
+   my $ip_list = $network_address->ipv4_list($subnet)
+      or return $self->log->error("scan: ipv4_list failed");
+
+   my $reply_cache = {};
+   my $local_arp_cache = {};
+   my @frame_list = ();
+
+   for my $ip (@$ip_list) {
+      # We scan ARP for everyone but our own IP
+      next if $ip eq $interface->{ipv4};
+
+      # XXX: move to network::arp so there is one place for ARP cache handling
+      my $mac;
+      if (exists($local_arp_cache->{$ip})) {
+         $mac = $local_arp_cache->{$ip};
+         $reply_cache->{$ip} = $mac;
+      }
+      elsif ($mac = $arp_cache->{$ip}) {
+         $self->log->verbose("scan: found mac [$mac] for ipv4 [$ip] in ARP cache");
+         $local_arp_cache->{$ip} = $mac;
+         $reply_cache->{$ip} = $mac;
+      }
+      else {
+         # If it is not in ARP cache yet
+         push @frame_list, $self->_get_arp_frame($ip);
+      }
+   }
+
+   my $network_write = Metabrik::Network::Write->new_from_brik_init($self) or return;
+
+   my $write = $network_write->open(2, $self->device)
+      or return $self->log->error("scan: open failed");
+
+   my $network_read = Metabrik::Network::Read->new_from_brik_init($self) or return;
+   $network_read->rtimeout($self->timeout);
+
+   my $filter = 'arp and src net '.$subnet.' and dst host '.$interface->{ipv4};
+   my $read = $network_read->open(2, $self->device, $filter)
+      or return $self->log->error("scan: open failed");
+
+   # We will send frames 3 times max
+   my $try = $self->try;
+   for my $t (1..$try) {
+      # We send all frames
+      for my $r (@frame_list) {
+         $self->debug && $self->log->debug($r->print);
+         my $dst_ip = $r->ref->{ARP}->dstIp;
+         if (! exists($reply_cache->{$dst_ip})) {
+            $network_write->send($r->raw)
+               or $self->log->warning("scan: send failed");
+         }
+      }
+
+      # Then we wait for all replies until a timeout occurs
+      my $h_list = $network_read->next_until_timeout;
+      for my $h (@$h_list) {
+         my $r = $self->from_read($h);
+         #$self->log->verbose("scan: read next returned some stuff".$r->print);
+
+         if ($r->ref->{ARP}->opCode != &Net::Frame::Layer::ARP::NF_ARP_OPCODE_REPLY) {
+            next;
+         }
+
+         my $src_ip = $r->ref->{ARP}->srcIp;
+         if (! exists($reply_cache->{$src_ip})) {
+            my $mac = $r->ref->{ARP}->src;
+            $self->log->info("scan: received mac [$mac] for ipv4 [$src_ip]");
+            $reply_cache->{$src_ip} = $r->ref->{ARP}->src;
+
+            # Put it in ARP cache table for next round
+            $local_arp_cache->{$src_ip} = $mac;
+         }
+      }
+
+      $network_read->reset_timeout;
+   }
+
+   $network_write->close;
+   $network_read->close;
+
+   my %results = ();
+   for (keys %$reply_cache) {
+      my $mac = $reply_cache->{$_};
+      my $ip4 = $_;
+      my $ip6 = $self->mac2eui64($mac);
+      $self->log->verbose(sprintf("%-16s => %s  [%s]", $ip4, $mac, $ip6));
+      $results{$ip4} = { ipv6 => $ip6, mac => $mac, ipv4 => $ip4 };
+      $results{$mac} = { ipv6 => $ip6, mac => $mac, ipv4 => $ip4 };
+      $results{$ip6} = { ipv6 => $ip6, mac => $mac, ipv4 => $ip4 };
+   }
+
+   return \%results;
 }
 
 1;
