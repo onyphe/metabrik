cef8ab77914d3027a9e6ce4071966247e1c76520
GomoR <gomor@metabrik.org>
2015-09-29 21:06:38 +0200

    - new: system::freebsd::jail: backup, restore, delete, update, name_to_id, exec Commands

--- lib/Metabrik/System/Freebsd/Jail.pm
+++ lib/Metabrik/System/Freebsd/Jail.pm
@@ -19,12 +19,83 @@ sub brik_properties {
          stop => [ qw(jail_name|$jail_list) ],
          restart => [ qw(jail_name|$jail_list) ],
          create => [ qw(jail_name ip_address) ],
+         backup => [ qw(jail_name|$jail_list) ],
+         restore => [ qw(jail_name ip_address archive_tar_gz) ],
+         delete => [ qw(jail_name) ],
+         update => [ ],
+         name_to_id => [ qw(jail_name) ],
+         exec => [ qw(jail_name command) ],
       },
       require_binaries => {
          'sudo' => [ ],
          'ezjail-admin' => [ ],
+         'jls' => [ ],
+         'jexec' => [ ],
       },
    };
+}
+
+sub name_to_id {
+   my $self = shift;
+   my ($jail_name) = @_;
+
+   if (! defined($jail_name)) {
+      return $self->log->error($self->brik_help_run('name_to_id'));
+   }
+
+   my $out = $self->capture("ezjail-admin list") or return;
+   # STA JID  IP              Hostname                       Root Directory
+   # --- ---- --------------- ------------------------------ ------------------------
+   # DR  13   192.168.X.Y     smtpout                        /usr/jails/smtpout
+   for my $line (@$out) {
+      #print "DEBUG line[$line]\n";
+      my @t = split(/\s+/, $line);
+      #print "DEBUG [@t]\n";
+      if (defined($t[1]) && $t[1] =~ /^\d+$/ && defined($t[3]) && $t[3] =~ /^$jail_name$/) {
+         return $t[1];
+      }
+   }
+
+   return $self->log->error("name_to_id: jail name not found");
+}
+
+sub exec {
+   my $self = shift;
+   my ($jail_name, $exec) = @_;
+
+   if (! defined($jail_name)) {
+      return $self->log->error($self->brik_help_run('exec'));
+   }
+   if (! defined($exec)) {
+      return $self->log->error($self->brik_help_run('exec'));
+   }
+
+   my $r;
+   my @lines = ();
+   if (ref($jail_name) eq 'ARRAY') {
+      for my $jail (@$jail_name) {
+         my $id = $self->name_to_id($jail) or next;
+         my $cmd = "sudo jexec $id $exec";
+         $r = $self->execute($cmd);
+         if ($self->capture_mode) {
+            push @lines, $r;
+         }
+      }
+   }
+   else {
+      my $id = $self->name_to_id($jail_name) or return;
+      my $cmd = "sudo jexec $id $exec";
+      $r = $self->execute($cmd);
+      if ($self->capture_mode) {
+         push @lines, $r;
+      }
+   }
+
+   if ($self->capture_mode) {
+      return \@lines;
+   }
+
+   return $r;
 }
 
 sub list {
@@ -32,7 +103,7 @@ sub list {
 
    my $cmd = "ezjail-admin list";
 
-   return $self->capture($cmd);
+   return $self->execute($cmd);
 }
 
 sub stop {
@@ -110,6 +181,75 @@ sub create {
    }
 
    my $cmd = "sudo ezjail-admin create $jail_name $ip_address";
+
+   return $self->system($cmd);
+}
+
+sub backup {
+   my $self = shift;
+   my ($jail_name) = @_;
+
+   if (! defined($jail_name)) {
+      return $self->log->error($self->brik_help_run('backup'));
+   }
+
+   if (ref($jail_name) eq 'ARRAY') {
+      for my $jail (@$jail_name) {
+         my $cmd = "sudo ezjail-admin archive -f $jail";
+         $self->system($cmd);
+      }
+      return 1;
+   }
+
+   my $cmd = "sudo ezjail-admin archive -f $jail_name";
+
+   return $self->system($cmd);
+}
+
+sub restore {
+   my $self = shift;
+   my ($jail_name, $ip_address, $archive_tar_gz) = @_;
+
+   if (! defined($jail_name)) {
+      return $self->log->error($self->brik_help_run('restore'));
+   }
+   if (! defined($ip_address)) {
+      return $self->log->error($self->brik_help_run('restore'));
+   }
+   if (! defined($archive_tar_gz)) {
+      return $self->log->error($self->brik_help_run('restore'));
+   }
+      
+   my $cmd = "sudo ezjail-admin create -a $archive_tar_gz $jail_name $ip_address";
+
+   return $self->system($cmd);
+}
+
+sub delete {
+   my $self = shift;
+   my ($jail_name) = @_;
+
+   if (! defined($jail_name)) {
+      return $self->log->error($self->brik_help_run('delete'));
+   }
+
+   if (ref($jail_name) eq 'ARRAY') {
+      for my $jail (@$jail_name) {
+         my $cmd = "sudo ezjail-admin delete -fw $jail";
+         $self->system($cmd);
+      }
+      return 1;
+   }
+
+   my $cmd = "sudo ezjail-admin delete -fw $jail_name";
+
+   return $self->system($cmd);
+}
+
+sub update {
+   my $self = shift;
+
+   my $cmd = "sudo ezjail-admin update -i";
 
    return $self->system($cmd);
 }
