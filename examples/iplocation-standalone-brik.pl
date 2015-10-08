1fc878472241763f49f095524589ad8e2cfb3773
GomoR <gomor@metabrik.org>
2015-10-08 10:03:02 +0200

    - new: file for dist: examples/iplocation-standalone-brik.pl

---
+++ examples/iplocation-standalone-brik.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+use Data::Dumper;
+use Metabrik::Core::Context;
+use Metabrik::Lookup::Iplocation;
+
+my $context = Metabrik::Core::Context->new;
+$context->brik_init or die("[FATAL] context init failed\n");
+
+my $li = Metabrik::Lookup::Iplocation->new_from_brik_init($context);
+print Dumper($li->from_ip("104.47.125.219"));
