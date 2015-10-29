65c802a1c1aac06f8aabcd611e1577a664857f15
GomoR <gomor@metabrik.org>
2015-10-29 22:23:32 +0100

    - new: system::process: new Brik

---
+++ lib/Metabrik/System/Process.pm
@@ -0,0 +1,144 @@
+#
+# $Id$
+#
+# system::process Brik
+#
+package Metabrik::System::Process;
+use strict;
+use warnings;
+
+use base qw(Metabrik::Shell::Command);
+
+sub brik_properties {
+   return {
+      revision => '$Revision$',
+      tags => [ qw(unstable system process) ],
+      attributes => {
+         force_kill => [ qw(0|1) ],
+      },
+      attributes_default => {
+         force_kill => 0,
+      },
+      commands => {
+         list => [ ],
+         is_running => [ qw(process) ],
+         get_process_info => [ qw(process) ],
+         kill => [ qw(process|pid) ],
+      },
+      require_binaries => {
+         'ps', => [ ],
+      },
+   };
+}
+
+sub list {
+   my $self = shift;
+
+   my $res = $self->capture("ps awuxw") or return;
+
+   my @a = ();
+   my $first = shift @$res;
+   my $header = [ split(/\s+/, $first) ];
+   my $count = scalar(@$header) - 1;
+   for my $this (@$res) {
+      my @toks = split(/\s+/, $this);
+      my $h = {};
+      for my $n (0..$count) {
+         $h->{$header->[$n]} = $toks[$n];
+      }
+      my $ntoks = scalar(@toks) - 1;
+      if ($ntoks > $count) {
+         for my $n (($count+1)..$ntoks) {
+            $h->{$header->[-1]} .= ' '.$toks[$n];
+         }
+      }
+      push @a, $h;
+   }
+
+   return \@a;
+}
+
+sub is_running {
+   my $self = shift;
+   my ($process) = @_;
+
+   if (! defined($process)) {
+      return $self->log->error($self->brik_help_run('is_running'));
+   }
+
+   my $list = $self->list or return;
+   for my $this (@$list) {
+      my $command = $this->{COMMAND};
+      my @toks = split(/\s+/, $command);
+      $toks[0] =~ s/^.*\/(.*?)$/$1/;
+      if ($toks[0] eq $process) {
+         return 1;
+      }
+   }
+
+   return 0;
+}
+
+sub get_process_info {
+   my $self = shift;
+   my ($process) = @_;
+
+   if (! defined($process)) {
+      return $self->log->error($self->brik_help_run('is_running'));
+   }
+
+   my @results = ();
+   my $list = $self->list or return;
+   for my $this (@$list) {
+      my $command = $this->{COMMAND};
+      my @toks = split(/\s+/, $command);
+      $toks[0] =~ s/^.*\/(.*?)$/$1/;
+      if ($toks[0] eq $process) {
+         push @results, $this;
+      }
+   }
+
+   return \@results;
+}
+
+sub kill {
+   my $self = shift;
+   my ($process) = @_;
+
+   if (! defined($process)) {
+      return $self->log->error($self->brik_help_run('kill'));
+   }
+
+   if ($process =~ /^\d+$/) {
+      kill('TERM', $process);
+   }
+   else {
+      my $list = $self->get_process_info($process) or return;
+      for my $this (@$list) {
+         kill('TERM', $this->{PID});
+      }
+   }
+
+   return 1;
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+Metabrik::System::Process - system::process Brik
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (c) 2014-2015, Patrice E<lt>GomoRE<gt> Auffret
+
+You may distribute this module under the terms of The BSD 3-Clause License.
+See LICENSE file in the source distribution archive.
+
+=head1 AUTHOR
+
+Patrice E<lt>GomoRE<gt> Auffret
+
+=cut
--- t/19-use-briks-system.t
+++ t/19-use-briks-system.t
@@ -1,5 +1,5 @@
 use Test;
-BEGIN { plan(tests => 7) }
+BEGIN { plan(tests => 8) }
 
 ok(sub { eval("use Metabrik::System::Os"); $@ ? 0 : 1 }, 1, $@);
 ok(sub { eval("use Metabrik::System::Package"); $@ ? 0 : 1 }, 1, $@);
@@ -8,3 +8,4 @@ ok(sub { eval("use Metabrik::System::Ubuntu::Package"); $@ ? 0 : 1 }, 1, $@);
 ok(sub { eval("use Metabrik::System::Freebsd::Package"); $@ ? 0 : 1 }, 1, $@);
 ok(sub { eval("use Metabrik::System::Freebsd::Jail"); $@ ? 0 : 1 }, 1, $@);
 ok(sub { eval("use Metabrik::System::Freebsd::Pf"); $@ ? 0 : 1 }, 1, $@);
+ok(sub { eval("use Metabrik::System::Process"); $@ ? 0 : 1 }, 1, $@);
