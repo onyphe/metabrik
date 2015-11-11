5a1015038231752ebbd47ea6755b6c9cba1bb4de
GomoR <gomor@metabrik.org>
2015-11-11 13:12:50 +0100

    - new: lookup::threalist: new Brik

---
+++ lib/Metabrik/Lookup/Threatlist.pm
@@ -0,0 +1,99 @@
+#
+# $Id$
+#
+# lookup::threatlist Brik
+#
+package Metabrik::Lookup::Threatlist;
+use strict;
+use warnings;
+
+use base qw(Metabrik);
+
+sub brik_properties {
+   return {
+      revision => '$Revision$',
+      tags => [ qw(unstable lookup ipv4 ipv6 ip threat threatlist) ],
+      attributes => {
+         datadir => [ qw(datadir) ],
+      },
+      commands => {
+         update => [ ],
+      },
+      require_modules => {
+         'Metabrik::Client::Www' => [ ],
+         'Metabrik::File::Compress' => [ ],
+      },
+   };
+}
+
+sub update {
+   my $self = shift;
+
+   my $datadir = $self->datadir;
+
+   my %mirror = (
+      'iblocklist-tgbankumtwtrzllndbmb.gz' => 'http://list.iblocklist.com/?list=logmein',
+      'iblocklist-nzldzlpkgrcncdomnttb.gz' => 'http://list.iblocklist.com/?list=nzldzlpkgrcncdomnttb',
+      'iblocklist-xoebmbyexwuiogmbyprb.gz' => 'http://list.iblocklist.com/?list=bt_proxy',
+      'iblocklist-zfucwtjkfwkalytktyiw.gz' => 'http://list.iblocklist.com/?list=zfucwtjkfwkalytktyiw',
+      'iblocklist-llvtlsjyoyiczbkjsxpf.gz' => 'http://list.iblocklist.com/?list=bt_spyware',
+      'iblocklist-togdoptykrlolpddwbvz.gz' => 'http://list.iblocklist.com/?list=tor',
+      'iblocklist-ghlzqtqxnzctvvajwwag.gz' => 'http://list.iblocklist.com/?list=ghlzqtqxnzctvvajwwag',
+      'sans-block.txt' => 'http://isc.sans.edu/block.txt',
+      'malwaredomains-domains.txt' => 'http://mirror1.malwaredomains.com/files/domains.txt',
+      'emergingthreats-compromised-ips.txt' => 'http://rules.emergingthreats.net/blockrules/compromised-ips.txt',
+      'emergingthreats-emerging-Block-IPs.txt' => 'http://rules.emergingthreats.net/fwrules/emerging-Block-IPs.txt',
+      'phishtank-verified_online.csv.gz' => 'http://data.phishtank.com/data/online-valid.csv.gz',
+      'abusech-palevotracker.txt' => 'https://palevotracker.abuse.ch/blocklists.php?download=ipblocklist',
+      'abusech-spyeyetracker.txt' => 'https://spyeyetracker.abuse.ch/blocklist.php?download=ipblocklist',
+      'abusech-zeustracker-badips.txt' => 'https://zeustracker.abuse.ch/blocklist.php?download=badips',
+      'abusech-zeustracker.txt' => 'https://zeustracker.abuse.ch/blocklist.php?download=ipblocklist',
+      'amazonaws-top-1m.csv.zip' => 'http://s3.amazonaws.com/alexa-static/top-1m.csv.zip',
+      'iana-tlds-alpha-by-domain.txt' => 'http://data.iana.org/TLD/tlds-alpha-by-domain.txt',
+      'publicsuffix-effective_tld_names.dat' => 'https://publicsuffix.org/list/effective_tld_names.dat',
+   );
+
+   my $cw = Metabrik::Client::Www->new_from_brik_init($self) or return;
+   $cw->user_agent("Metabrik-Lookup-Threatlist-mirror/1.00");
+   $cw->datadir($datadir);
+
+   my $fc = Metabrik::File::Compress->new_from_brik_init($self) or return;
+   $fc->datadir($datadir);
+
+   for my $f (keys %mirror) {
+      my $files = $cw->mirror($mirror{$f}, $f) or next;
+      for my $file (@$files) {
+         if ($file =~ /\.gz$/) {
+            (my $outfile = $file) =~ s/\.gz$//;
+            $fc->uncompress($datadir.'/'.$file, $outfile);
+         }
+         elsif ($file =~ /\.zip$/) {
+            (my $outfile = $file) =~ s/\.zip$//;
+            $fc->uncompress($datadir.'/'.$file, $outfile);
+         }
+      }
+   }
+
+   return 1;
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+Metabrik::Lookup::Threatlist - lookup::threatlist Brik
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (c) 2014-2015, Patrice E<lt>GomoRE<gt> Auffret
+
+You may distribute this module under the terms of The BSD 3-Clause License.
+See LICENSE file in the source distribution archive.
+
+=head1 AUTHOR
+
+Patrice E<lt>GomoRE<gt> Auffret
+
+=cut
--- t/31-use-briks-lookup.t
+++ t/31-use-briks-lookup.t
@@ -1,5 +1,5 @@
 use Test;
-BEGIN { plan(tests => 8) }
+BEGIN { plan(tests => 9) }
 
 ok(sub { eval("use Metabrik::Lookup::Ethernet"); $@ ? 0 : 1 }, 1, $@);
 ok(sub { eval("use Metabrik::Lookup::Ip"); $@ ? 0 : 1 }, 1, $@);
@@ -9,3 +9,4 @@ ok(sub { eval("use Metabrik::Lookup::Oui"); $@ ? 0 : 1 }, 1, $@);
 ok(sub { eval("use Metabrik::Lookup::Tcpoption"); $@ ? 0 : 1 }, 1, $@);
 ok(sub { eval("use Metabrik::Lookup::Ipoption"); $@ ? 0 : 1 }, 1, $@);
 ok(sub { eval("use Metabrik::Lookup::Iplocation"); $@ ? 0 : 1 }, 1, $@);
+ok(sub { eval("use Metabrik::Lookup::Threatlist"); $@ ? 0 : 1 }, 1, $@);
