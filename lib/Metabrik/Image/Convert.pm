8b52acc68b9c3d514b1bdc531928d2acf5a0b37b
GomoR <gomor@metabrik.org>
2015-12-27 17:13:35 +0100

    - new: image::convet: new Brik

---
+++ lib/Metabrik/Image/Convert.pm
@@ -0,0 +1,72 @@
+#
+# $Id$
+#
+# image::convert Brik
+#
+package Metabrik::Image::Convert;
+use strict;
+use warnings;
+
+use base qw(Metabrik::Shell::Command Metabrik::System::Package);
+
+sub brik_properties {
+   return {
+      revision => '$Revision$',
+      tags => [ qw(unstable) ],
+      author => 'GomoR <GomoR[at]metabrik.org>',
+      license => 'http://opensource.org/licenses/BSD-3-Clause',
+      attributes => {
+         output => [ qw(file) ],
+         delay => [ qw(microseconds) ],
+      },
+      attributes_default => {
+         output => 'output.gif',
+         delay => 50,
+      },
+      commands => {
+         install => [ ], # Inherited
+         to_animated_gif => [ qw($files output|OPTIONAL delay|OPTIONAL) ],
+      },
+      require_binaries => {
+         'convert' => [ ],
+      },
+      need_packages => {
+         'ubuntu' => [ qw(imagemagick) ],
+      },
+   };
+}
+
+sub to_animated_gif {
+   my $self = shift;
+   my ($files, $output, $delay) = @_;
+
+   $output ||= $self->output;
+   $delay ||= $self->delay;
+   $self->brik_help_run_undef_arg('to_animated_gif', $files) or return;
+   $self->brik_help_run_invalid_arg('to_animated_gif', $files, 'ARRAY') or return;
+   $self->brik_help_run_empty_array_arg('to_animated_gif', $files) or return;
+
+   my $file_list = join(' ', @$files);
+   return $self->execute("convert -delay $delay -loop 0 $file_list $output");
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+Metabrik::Image::Convert - image::convert Brik
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (c) 2014-2015, Patrice E<lt>GomoRE<gt> Auffret
+
+You may distribute this module under the terms of The BSD 3-Clause License.
+See LICENSE file in the source distribution archive.
+
+=head1 AUTHOR
+
+Patrice E<lt>GomoRE<gt> Auffret
+
+=cut
