181d7ed47d2f049d061eb020933a6676d7d5de92
GomoR <gomor@metabrik.org>
2016-01-18 11:54:12 +0100

    - new: crypto::letsencrypt: new Brik - new: devel::git: new Brik

---
+++ lib/Metabrik/Devel/Git.pm
@@ -0,0 +1,80 @@
+#
+# $Id$
+#
+# devel::git Brik
+#
+package Metabrik::Devel::Git;
+use strict;
+use warnings;
+
+use base qw(Metabrik::Shell::Command Metabrik::System::Package);
+
+sub brik_properties {
+   return {
+      revision => '$Revision$',
+      tags => [ qw(unstable) ],
+      author => 'GomoR <GomoR[at]metabrik.org>',
+      license => 'http://opensource.org/licenses/BSD-3-Clause',
+      attributes => {
+         datadir => [ qw(datadir) ],
+         capture_mode => [ qw(0|1) ],
+         use_sudo => [ qw(0|1) ],
+         use_pager => [ qw(0|1) ],
+      },
+      attributes_default => {
+         capture_mode => 0,
+         use_sudo => 0,
+         use_pager => 1,
+      },
+      commands => {
+         install => [ ], # Inherited
+         clone => [ qw(repository directory|OPTIONAL) ],
+      },
+      require_binaries => {
+         git => [ ],
+      },
+      need_packages => {
+         ubuntu => [ qw(git) ],
+      },
+   };
+}
+
+sub clone {
+   my $self = shift;
+   my ($repository, $directory) = @_;
+
+   $self->brik_help_run_undef_arg('clone', $repository) or return;
+
+   if (! defined($directory)) {
+      my $datadir = $self->datadir;
+
+      $directory ||= $datadir;
+      my ($name) = $repository =~ m{^.*/(.*)$};
+      $directory .= '/'.$name;
+   }
+
+   my $cmd = "git clone $repository $directory";
+
+   return $self->execute($cmd);
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+Metabrik::Devel::Git - devel::git Brik
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (c) 2014-2016, Patrice E<lt>GomoRE<gt> Auffret
+
+You may distribute this module under the terms of The BSD 3-Clause License.
+See LICENSE file in the source distribution archive.
+
+=head1 AUTHOR
+
+Patrice E<lt>GomoRE<gt> Auffret
+
+=cut
