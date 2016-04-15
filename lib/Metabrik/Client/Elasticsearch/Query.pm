87cb26f1ea0be735557431c25a3940add1200935
GomoR <gomor@metabrik.org>
2016-04-15 08:20:17 +0200

    - update: client::elasticsearch::query: converted Argument passing to key=value

--- lib/Metabrik/Client/Elasticsearch/Query.pm
+++ lib/Metabrik/Client/Elasticsearch/Query.pm
@@ -16,8 +16,11 @@ sub brik_properties {
       author => 'GomoR <GomoR[at]metabrik.org>',
       license => 'http://opensource.org/licenses/BSD-3-Clause',
       attributes => {
+         nodes => [ qw(node_list) ], # Inherited
          index => [ qw(index) ],     # Inherited
          type => [ qw(type) ],       # Inherited
+         from => [ qw(number) ],     # Inherited
+         size => [ qw(count) ],      # Inherited
          client => [ qw(INTERNAL) ],
       },
       attributes_default => {
@@ -31,10 +34,10 @@ sub brik_properties {
          get_query_result_hits => [ qw($query_result) ],
          get_query_result_timed_out => [ qw($query_result) ],
          get_query_result_took => [ qw($query_result) ],
-         term => [ qw(field value index|OPTIONAL type|OPTIONAL) ],
-         range => [ qw(field_from field_to value index|OPTIONAL type|OPTIONAL) ],
-         top => [ qw(field count index|OPTIONAL type|OPTIONAL) ],
-         top_match => [ qw(field count field2 match index|OPTIONAL type|OPTIONAL) ],
+         term => [ qw(kv index|OPTIONAL type|OPTIONAL) ],
+         range => [ qw(kv_from kv_to value index|OPTIONAL type|OPTIONAL) ],
+         top => [ qw(kv_count index|OPTIONAL type|OPTIONAL) ],
+         top_match => [ qw(kv_count kv_match index|OPTIONAL type|OPTIONAL) ],
       },
    };
 }
@@ -140,25 +143,29 @@ sub _query {
 }
 
 #
-# run client::elasticsearch::query term domain example.com index1-*,index2-*
+# run client::elasticsearch::query term domain=example.com index1-*,index2-*
 #
 sub term {
    my $self = shift;
-   my ($field, $value, $index, $type) = @_;
+   my ($kv, $index, $type) = @_;
 
    $index ||= $self->index;
    $type ||= $self->type;
-   $self->brik_help_run_undef_arg('term', $field) or return;
-   $self->brik_help_run_undef_arg('term', $value) or return;
+   $self->brik_help_run_undef_arg('term', $kv) or return;
    $self->brik_help_run_undef_arg('term', $index) or return;
    $self->brik_help_run_undef_arg('term', $type) or return;
+
+   if ($kv !~ /^\S+=\S+$/) {
+      return $self->log->error("term: kv must be in the form 'key=value'");
+   }
+   my ($key, $value) = split('=', $kv);
 
    my $ce = $self->create_client or return;
 
    my $q = {
       query => {
          term => {
-            $field => $value,
+            $key => $value,
          },
       },
    };
@@ -167,19 +174,27 @@ sub term {
 }
 
 #
-# run client::elasticsearch::query range ip_range.from ip_range.to 192.168.255.36
+# run client::elasticsearch::query range ip_range.from=192.168.255.36 ip_range.to=192.168.255.36
 #
 sub range {
    my $self = shift;
-   my ($field_from, $field_to, $value, $index, $type) = @_;
+   my ($kv_from, $kv_to, $index, $type) = @_;
 
    $index ||= $self->index;
    $type ||= $self->type;
-   $self->brik_help_run_undef_arg('range', $field_from) or return;
-   $self->brik_help_run_undef_arg('range', $field_to) or return;
-   $self->brik_help_run_undef_arg('range', $value) or return;
+   $self->brik_help_run_undef_arg('range', $kv_from) or return;
+   $self->brik_help_run_undef_arg('range', $kv_to) or return;
    $self->brik_help_run_undef_arg('range', $index) or return;
    $self->brik_help_run_undef_arg('range', $type) or return;
+
+   if ($kv_from !~ /^\S+=\S+$/) {
+      return $self->log->error("term: kv_from [$kv_from] must be in the form 'key=value'");
+   }
+   if ($kv_to !~ /^\S+=\S+$/) {
+      return $self->log->error("term: kv_to [$kv_to] must be in the form 'key=value'");
+   }
+   my ($key_from, $value_from) = split('=', $kv_from);
+   my ($key_to, $value_to) = split('=', $kv_to);
 
    my $ce = $self->create_client or return;
 
@@ -188,8 +203,8 @@ sub range {
          constant_score => {
             filter => {
                and => [
-                  { range => { $field_to => { gte => $value } } },
-                  { range => { $field_from => { lte => $value } } },
+                  { range => { $key_to => { gte => $value_to } } },
+                  { range => { $key_from => { lte => $value_from } } },
                ],
             },
          },
@@ -200,18 +215,22 @@ sub range {
 }
 
 #
-# run client::elasticsearch::query top name 10 users-*
+# run client::elasticsearch::query top name=10 users-*
 #
 sub top {
    my $self = shift;
-   my ($field, $count, $index, $type) = @_;
+   my ($kv_count, $index, $type) = @_;
 
    $index ||= $self->index;
    $type ||= $self->type;
-   $self->brik_help_run_undef_arg('top', $field) or return;
-   $self->brik_help_run_undef_arg('top', $count) or return;
+   $self->brik_help_run_undef_arg('top', $kv_count) or return;
    $self->brik_help_run_undef_arg('top', $index) or return;
    $self->brik_help_run_undef_arg('top', $type) or return;
+
+   if ($kv_count !~ /^\S+=\S+$/) {
+      return $self->log->error("term: kv_count [$kv_count] must be in the form 'key=value'");
+   }
+   my ($key_count, $value_count) = split('=', $kv_count);
 
    my $ce = $self->create_client or return;
 
@@ -219,8 +238,8 @@ sub top {
       aggs => {
          top_values => {
             terms => {
-               field => $field,
-               size => int($count),
+               field => $key_count,
+               size => int($value_count),
             },
          },
       },
@@ -229,32 +248,42 @@ sub top {
    return $self->_query($q, $index, $type);
 }
 
+#
+# run client::elasticsearch::query top_match domain=10 host=*www*
+#
 sub top_match {
    my $self = shift;
-   my ($field, $count, $field2, $match, $index, $type) = @_;
+   my ($kv_count, $kv_match, $index, $type) = @_;
 
    $index ||= $self->index;
    $type ||= $self->type;
-   $self->brik_help_run_undef_arg('top_match', $field) or return;
-   $self->brik_help_run_undef_arg('top_match', $count) or return;
-   $self->brik_help_run_undef_arg('top_match', $field2) or return;
-   $self->brik_help_run_undef_arg('top_match', $match) or return;
+   $self->brik_help_run_undef_arg('top_match', $kv_count) or return;
+   $self->brik_help_run_undef_arg('top_match', $kv_match) or return;
    $self->brik_help_run_undef_arg('top_match', $index) or return;
    $self->brik_help_run_undef_arg('top_match', $type) or return;
+
+   if ($kv_count !~ /^\S+=\S+$/) {
+      return $self->log->error("term: kv_count [$kv_count] must be in the form 'key=value'");
+   }
+   if ($kv_match !~ /^\S+=\S+$/) {
+      return $self->log->error("term: kv_match [$kv_match] must be in the form 'key=value'");
+   }
+   my ($key_count, $value_count) = split('=', $kv_count);
+   my ($key_match, $value_match) = split('=', $kv_match);
 
    my $ce = $self->create_client or return;
 
    my $q = {
       query => {
          match => {
-            $field2 => $match,
+            $key_match => $value_match,
          },
       },
       aggs => {
          top_values => {
             terms => {
-               field => $field,
-               size => int($count),
+               field => $key_count,
+               size => int($value_count),
             },
          },
       },
