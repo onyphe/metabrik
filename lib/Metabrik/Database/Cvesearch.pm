91dc2119a373b5281c0e4104ba7b0891f9ce1ea1
GomoR <gomor@metabrik.org>
2016-09-27 08:49:02 +0200

    - update: database::cvesearch: cpe_search, cve_search, dump_last new Commands

--- lib/Metabrik/Database/Cvesearch.pm
+++ lib/Metabrik/Database/Cvesearch.pm
@@ -23,9 +23,14 @@ sub brik_properties {
          init_database => [ ],
          update_database => [ ],
          repopulate_database => [ ],
+         cpe_search => [ qw(cpe) ],
+         cve_search => [ qw(cve) ],
+         dump_last => [ ],
       },
       require_modules => {
          'Metabrik::Devel::Git' => [ ],
+         'Metabrik::String::Json' => [ ],
+         'Metabrik::String::Xml' => [ ],
       },
       require_binaries => {
          python3 => [ ],
@@ -36,6 +41,10 @@ sub brik_properties {
       },
    };
 }
+
+#
+# https://github.com/cve-search/cve-search
+#
 
 sub brik_use_properties {
    my $self = shift;
@@ -104,6 +113,102 @@ sub repopulate_database {
    }
 
    return 1;
+}
+
+#
+# run database::cvesearch cpe_search cisco:ios:12.4
+#
+sub cpe_search {
+   my $self = shift;
+   my ($cpe) = @_;
+
+   $self->brik_help_run_undef_arg('search_cpe', $cpe) or return;
+
+   my $repo = $self->repo;
+
+   my $cmd = $repo.'/bin/search.py -o json -p '.$cpe;
+
+   my $json = $self->capture($cmd) or return;
+   if (@$json <= 0) {
+      return $self->log->error("search_cpe: invalid response: ".join('', @$json));
+   }
+
+   my $sj = Metabrik::String::Json->new_from_brik_init($self) or return;
+
+   my @results = ();
+   for my $this (@$json) {
+      my $r = $sj->decode($this) or next;
+      push @results, $r;
+   }
+
+   return \@results;
+}
+
+#
+# run database::cvesearch cve_search CVE-2010-3333
+#
+sub cve_search {
+   my $self = shift;
+   my ($cve) = @_;
+
+   $self->brik_help_run_undef_arg('search_cve', $cve) or return;
+
+   if ($cve !~ m{^(cve|can)\-\d+\-\d+$}i) {
+      return $self->log->error("search_cve: invalid CVE format [$cve]");
+   }
+
+   my $repo = $self->repo;
+
+   my $cmd = $repo.'/bin/search.py -o json -c '.$cve;
+
+   my $json = $self->capture($cmd) or return;
+   if (@$json <= 0) {
+      return $self->log->error("search_cve: invalid response: ".join('', @$json));
+   }
+
+   my $sj = Metabrik::String::Json->new_from_brik_init($self) or return;
+
+   my @results = ();
+   for my $this (@$json) {
+      my $r = $sj->decode($this) or next;
+      push @results, $r;
+   }
+
+   return \@results;
+}
+
+sub dump_last {
+   my $self = shift;
+
+   my $repo = $self->repo;
+
+   my $cmd = $repo.'/bin/dump_last.py -f atom';
+
+   my $xml = $self->capture($cmd) or return;
+   if (@$xml <= 0) {
+      return $self->log->error("last_entries: invalid response: ".join('', @$xml));
+   }
+
+   $xml = join('', @$xml);
+
+   my $sx = Metabrik::String::Xml->new_from_brik_init($self) or return;
+
+   my $decoded = $sx->decode($xml) or next;
+   my $h = $decoded->{entry};
+
+   my @results = ();
+   for my $k (keys %$h) {
+      my $title = $h->{$k}{title};
+      my ($cve) = $title =~ m{^\s*(CVE\-\d+\-\d+)\s+}i;
+      $h->{$k}{cve} = $cve;
+
+      my $href = $h->{$k}{link}{href};
+      $h->{$k}{link} = $href;
+
+      push @results, $h->{$k};
+   }
+
+   return \@results;
 }
 
 1;
