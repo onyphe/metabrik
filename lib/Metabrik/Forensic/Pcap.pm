a46eedff0dd5895a1eeaafe8bc61cea4a747cf79
GomoR <gomor@metabrik.org>
2017-02-25 19:04:30 +0100

    - new: forensic::pcap: new Brik

---
+++ lib/Metabrik/Forensic/Pcap.pm
@@ -0,0 +1,193 @@
+#
+# $Id$
+#
+# forensic::pcap Brik
+#
+package Metabrik::Forensic::Pcap;
+use strict;
+use warnings;
+
+use base qw(Metabrik::Client::Elasticsearch::Query);
+
+sub brik_properties {
+   return {
+      revision => '$Revision$',
+      tags => [ qw(unstable) ],
+      author => 'GomoR <GomoR[at]metabrik.org>',
+      license => 'http://opensource.org/licenses/BSD-3-Clause',
+      attributes => {
+         nodes => [ qw(node_list) ], # Inherited
+         index => [ qw(index) ],     # Inherited
+         type => [ qw(type) ],       # Inherited
+      },
+      attributes_default => {
+         index => 'forensicpcap-*',
+         type => 'pcap',
+      },
+      commands => {
+         create_client => [ ],  # Inherited
+         reset_client => [ ],  # Inherited
+         query => [ qw(query index|OPTIONAL type|OPTIONAL) ], # Inherited
+         from_json_file => [ qw(json_file index|OPTIONAL type|OPTIONAL) ], # Inherited
+         from_dump_file => [ qw(dump_file index|OPTIONAL type|OPTIONAL) ], # Inherited
+         pcap_to_elasticsearch => [ qw(file filter|OPTIONAL) ],
+      },
+      require_modules => {
+         'Metabrik::File::Pcap' => [ ],
+         'Metabrik::Time::Universal' => [ ],
+      },
+      require_binaries => {
+      },
+      optional_binaries => {
+      },
+      need_packages => {
+      },
+   };
+}
+
+sub brik_use_properties {
+   my $self = shift;
+
+   return {
+      attributes_default => {
+      },
+   };
+}
+
+sub brik_preinit {
+   my $self = shift;
+
+   # Do your preinit here, return 0 on error.
+
+   return $self->SUPER::brik_preinit;
+}
+
+sub brik_init {
+   my $self = shift;
+
+   # Do your init here, return 0 on error.
+
+   return $self->SUPER::brik_init;
+}
+
+sub pcap_to_elasticsearch {
+   my $self = shift;
+   my ($file, $filter) = @_;
+
+   $filter ||= '';
+   $self->brik_help_run_undef_arg('pcap_to_elasticsearch', $file) or return;
+   $self->brik_help_run_file_not_found('pcap_to_elasticsearch', $file) or return;
+
+   my $fp = Metabrik::File::Pcap->new_from_brik_init($self) or return;
+   $fp->open($file, 'read', $filter) or return;
+
+   my $tu = Metabrik::Time::Universal->new_from_brik_init($self) or return;
+   my $today = $tu->today;
+
+   $self->create_client or return;
+
+   $self->open_bulk_mode("forensicpcap-$today", "pcap") or return;
+
+   my $print_re = qr/^[[:print:]]{5,}/;
+
+   while (1) {
+      my $h = $fp->read_next(10); # We read 10 by 10
+      if (! defined($h)) {
+         $self->log->error("pcap_to_elasticsearch: unable to read frame, skipping");
+         next;
+      }
+
+      last if @$h == 0; # Eof
+
+      for my $this (@$h) {
+         my $simple = $fp->from_read($this);
+         if (! defined($simple)) {
+            $self->log->error("pcap_to_elasticsearch: unable to parse frame, skipping");
+            next;
+         }
+
+         my $timestamp = $simple->timestamp;
+
+         my $new = {
+            '@version' => 1,
+            '@timestamp' => $tu->timestamp_to_tz_time($timestamp),
+         };
+         my $skip_payload = 0;
+         for my $layer (reverse $simple->layers) {
+            my $this_layer = $layer->layer;
+
+            my $class = ref($layer);
+            my %h = map { $_ => $layer->[$layer->cgGetIndice($_)] }
+               @{$class->cgGetAttributes};
+
+            # The first we loop, we are using the last layer where we 
+            # want to keep the payload. For all others, we remove it.
+            if ($skip_payload) {
+               delete $h{payload};
+            }
+            else {
+               $skip_payload = 1;
+            }
+            # We convert IPv4 and TCP options to hex
+            if ($layer->layer eq 'TCP') {
+               $h{options} = CORE::unpack('H*', $h{options});
+               # If payload does not seem printable, we encode in hex
+               if (defined($h{payload}) && $h{payload} !~ $print_re) {
+                  $h{payload} = CORE::unpack('H*', $h{payload});
+               }
+               #print "payload[".$h{payload}."]\n" if defined($h{payload});
+            }
+            if ($layer->layer eq 'UDP') {
+               # If payload does not seem printable, we encode in hex
+               if (defined($h{payload}) && $h{payload} !~ $print_re) {
+                  $h{payload} = CORE::unpack('H*', $h{payload});
+               }
+               #print "payload[".$h{payload}."]\n" if defined($h{payload});
+            }
+            elsif ($layer->layer eq 'IPv4') {
+               $h{options} = CORE::unpack('H*', $h{options});
+            }
+            delete $h{raw};
+            delete $h{nextLayer};
+            delete $h{noFixLen};
+
+            $new->{$this_layer} = \%h;
+         }
+
+         $self->index_bulk($new);
+      }
+   }
+
+   $self->bulk_flush;
+
+   return 1;
+}
+
+sub brik_fini {
+   my $self = shift;
+
+   # Do your fini here, return 0 on error.
+
+   return $self->SUPER::brik_fini;
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+Metabrik::Forensic::Pcap - forensic::pcap Brik
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (c) 2014-2017, Patrice E<lt>GomoRE<gt> Auffret
+
+You may distribute this module under the terms of The BSD 3-Clause License.
+See LICENSE file in the source distribution archive.
+
+=head1 AUTHOR
+
+Patrice E<lt>GomoRE<gt> Auffret
+
+=cut
--- t/08-use-briks-forensic.t
+++ t/08-use-briks-forensic.t
@@ -1,7 +1,8 @@
 use Test;
-BEGIN { plan(tests => 4) }
+BEGIN { plan(tests => 5) }
 
 ok(sub { eval("use Metabrik::Forensic::Dcfldd"); $@ ? 0 : 1 }, 1, $@);
 ok(sub { eval("use Metabrik::Forensic::Scalpel"); $@ ? 0 : 1 }, 1, $@);
 ok(sub { eval("use Metabrik::Forensic::Volatility"); $@ ? 0 : 1 }, 1, $@);
 ok(sub { eval("use Metabrik::Forensic::Sysmon"); $@ ? 0 : 1 }, 1, $@);
+ok(sub { eval("use Metabrik::Forensic::Pcap"); $@ ? 0 : 1 }, 1, $@);
